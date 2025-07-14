73efd224499ffd2f075d65ed68fa29b99d80876a
Niete <nieteis@gmail.com>
2025-07-14 22:27:34 +0100

    Added allComicQuery to the queries ts

--- nextjs-comic-store/src/sanity/queries.ts
+++ nextjs-comic-store/src/sanity/queries.ts
@@ -2,6 +2,25 @@ export const allComicsQuery = `
   *[_type == "comic"]{
     _id,
     title,
+    issue,
+    slug,
+    description,
+    price,
+    stock,
+    releaseDate,
+    "coverImage": coverImage.asset->url,
+    "categories": categories[]->title,
+    "authors": authors[]->name,
+    "artists": artists[]->name,
+    "publisher": publisher->name
+  }
+`;
+
+
+export const comicBySlugQuery = `
+  *[_type == "comic" && slug.current == $slug][0]{
+    _id,
+    title,
     slug,
     description,
     price,
