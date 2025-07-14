1ac33fd843df212a2cdb91116ee9a612aed2a06c
Niete <nieteis@gmail.com>
2025-07-14 22:13:44 +0100

    Defined a schema for author

---
+++ studio-comic-store/schemaTypes/author.ts
@@ -0,0 +1,10 @@
+import {defineField, defineType} from 'sanity'
+
+export default defineType({
+  name: 'author',
+  title: 'Author',
+  type: 'document',
+  fields: [
+    defineField({ name: 'name', title: 'Name(s)', type: 'string' }),
+  ],
+});
